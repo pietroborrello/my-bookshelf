4f21661ccee138a6bcf96322ebd3f68ebe265a44
pietroborrello <pietro.borrello95@gmail.com>
2019-11-14 13:08:49 +0100

    maybe functioning search

--- js/classes.js
+++ js/classes.js
@@ -12,16 +12,24 @@ Item.prototype.toString = function() {
 
 
 class Bookshelf{
-    constructor(item2tags, tag2items){
+    constructor(item2tags, tag2items, word2items){
         this.item2tags = item2tags;
         this.tag2items = tag2items;
+        this.word2items = word2items;
     }
 
 }
 
 Bookshelf.prototype.getItemsFromTag = function (tag) {
-    if (! (tag in this.tag2items)){
+    if (!(this.tag2items.has(tag))){
         return [];
     }
-    return this.tag2items[tag];
+    return this.tag2items.get(tag);
+};
+
+Bookshelf.prototype.getItemsFromWord = function (word) {
+    if (!(this.word2items.has(word))) {
+        return new Set();
+    }
+    return this.word2items.get(word);
 };
